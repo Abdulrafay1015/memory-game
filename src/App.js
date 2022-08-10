83d3cd36d4f8437adc67aa32b848e64b3dbd1422
Abdulrafay1015 <summaiyajunaid6@gmail.com>
2022-08-10 21:10:01 +0500

    complete functionality added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,83 @@
-import { useState } from 'react';
-import './App.css';
-import SingleCard from './components/singleCard';
-
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/singleCard";
 
 const card_image = [
-  { src: "/img/sword-1.png" },
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" , match : false},
+  { src: "/img/helmet-1.png" , match : false},
+  { src: "/img/potion-1.png" , match : false},
+  { src: "/img/ring-1.png" , match : false},
+  { src: "/img/scroll-1.png" , match : false},
+  { src: "/img/shield-1.png" , match : false} 
 ];
 
-
 function App() {
-  
   const [cards, setCards] = useState([]);
-  const [turns,setTurns] = useState(0);
-
-
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   //shuffle Cards
   const shuffleCards = () => {
-    const shuffle = [...card_image,...card_image]
-    .sort( () => Math.random() - 0.5)
-    .map((card) => ({...card , id : Math.random()}))
+    const shuffle = [...card_image, ...card_image]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffle);
-    setTurns(0); 
+    setChoiceOne(null)
+    setChoiceTwo(null);
+    setTurns(0);
+  };
+
+  //Handle Click
+
+  const makeChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+
+  //match cards
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              
+              return  {...card , match : true}
+
+            } 
+            else {
+              return card
+            }
+          })
+        })
+        resetTurns()
+      }
+      else {
+        setTimeout(()=> 
+          resetTurns(),1000  
+        )
+      }
+    } 
+
+  }, [choiceOne,choiceTwo])
+
+  //START GAME Automatically
+useEffect(()=> {
+  shuffleCards()
+},[])
+
+  const resetTurns = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setDisabled(false)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
-  console.log(cards, turns)
-  
-  
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -41,9 +85,16 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            makeChoice={makeChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.match}
+            disabled={disabled}
+          />
         ))}
       </div>
+      <p>Turns : {turns}</p>
     </div>
   );
 }
--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -1,14 +1,26 @@
-import "./singleCard.css"
+import "./singleCard.css";
 
-const SingleCard = ({ card }) => {
-    return (
-      <div className="card">
-        <div>
-          <img src={card.src} className="front" alt="front" />
-          <img src="/img/cover.png" className="back" alt="back" />
-        </div>
+const SingleCard = ({ card, makeChoice , flipped , disabled }) => {
+  
+    const handleClick = () => {
+      if (!disabled) {
+          makeChoice(card);
+      }
+    };
+
+  return (
+    <div className="card">
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} className="front" alt="front" />
+        <img
+          src="/img/cover.png"
+          className="back"
+          alt="back"
+          onClick={handleClick}
+        />
       </div>
-    );
-}
- 
+    </div>
+  );
+};
+
 export default SingleCard;
